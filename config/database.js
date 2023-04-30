21b5706a76f6ecea58ea0d98dea7dfd743b1b38a
A3Barbosa <a3barbosa21@gmail.com>
2023-04-30 10:42:38 -0400

    login auth1

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://a3barbosa21:stackup@cluster0.7wqjjgy.mongodb.net/authOne?retryWrites=true&w=majority', 
+    'dbName': 'authOne'
+};
